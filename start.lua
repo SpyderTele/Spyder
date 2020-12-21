cf377fc07f6e5406e385f92f167556bac78209f6
SpyderTele <76131660+SpyderTele@users.noreply.github.com>
2020-12-22 02:42:07 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -71,7 +71,7 @@ RunSpyder:write([[
 cd $HOME/Spyder
 token="]]..database:get(Server_Spyder.."Token_Spyder")..[["
 rm -fr Spyder.lua
-wget "https://raw.githubusercontent.com/korapica-Team/Spyder/master/Spyder.lua"
+wget "https://raw.githubusercontent.com/SpyderTele/Spyder/main/Spyder.lua"
 while(true) do
 rm -fr ../.telegram-cli
 ./tg -s ./Spyder.lua -p PROFILE --bot=$token
