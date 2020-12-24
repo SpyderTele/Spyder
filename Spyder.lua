1a90a40e5bcb0c08298e4eaf3aac5212a3c9b688
SpyderTele <76131660+SpyderTele@users.noreply.github.com>
2020-12-24 04:15:14 +0300

    Add files via upload

--- Spyder.lua
+++ Spyder.lua
@@ -7461,7 +7461,7 @@ Text = [[
 ]]
 send(msg.chat_id_, msg.id_,Text)
 end
-if text == 'رابط الحذف' or text == 'رابط حذف' or text == 'بوت الحذف' then  
+if text == 'رابط الحذف' or text == 'بوت الحذف' or text == 'رابط حذف' then  
 if AddChannel(msg.sender_user_id_) == false then
 local textchuser = database:get(bot_id..'text:ch:user')
 if textchuser then
